5706908b377f40d59d265b5b980d07b13c4b8dda
SamoylovaLana <samoylovalana@yandex.ru>
2023-05-31 11:36:28 +0400

    Added

--- index.js
+++ index.js
@@ -46,18 +46,108 @@ function renderApp() {
 }
 renderApp();
 
+const gameHeader = ` <div class="game__header">
+<div class="game__time-box">
+    <div class="game__text-box">
+        <p class="game__text">min</p>
+        <p class="game__text">sek</p>
+    </div>
+    <p class="game__time">00.00</p>
+</div>
+<div class="button">Начать заново</div>
+</div>`;
+
+const gameClosedCards = `
+<div class="game">
+  ${gameHeader}
+  <div class="game__cards">
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+<div class="game__cards-item"></div>
+</div>
+</div>`;
+
+const gameOpenCards = `<div class="game">
+      ${gameHeader}
+<div class="game__cards">
+<div class="game__cards-item"><img src="./img/cards/туз пики.png" alt="король пики"></div>
+<div class="game__cards-item"><img src="./img/cards/король пики.png" alt="король пики"></div>
+<div class="game__cards-item"><img src="./img/cards/дама пики.png" alt="дама пики"></div>
+<div class="game__cards-item"><img src="./img/cards/валет пики.png" alt="валет пики"></div>
+<div class="game__cards-item"><img src="./img/cards/10 пики.png" alt="10 пики"></div>
+<div class="game__cards-item"><img src="./img/cards/9 пики.png" alt="9 пики"></div>
+<div class="game__cards-item"><img src="./img/cards/8 пики.png" alt="8 пики"></div>
+<div class="game__cards-item"><img src="./img/cards/7 пики.png" alt="7 пики"></div>
+<div class="game__cards-item"><img src="./img/cards/6 пики.png" alt="6 пики"></div>
+<div class="game__cards-item"><img src="./img/cards/туз черви.png" alt="туз черви"></div>
+<div class="game__cards-item"><img src="./img/cards/король черви.png" alt="король черви"></div>
+<div class="game__cards-item"><img src="./img/cards/дама черви.png" alt="дама черви"></div>
+<div class="game__cards-item"><img src="./img/cards/валет черви.png" alt="валет черви"></div>
+<div class="game__cards-item"><img src="./img/cards/10 черви.png" alt="10 черви"></div>
+<div class="game__cards-item"><img src="./img/cards/9 черви.png" alt="9 черви"></div>
+<div class="game__cards-item"><img src="./img/cards/8 черви.png" alt="8 черви"></div>
+<div class="game__cards-item"><img src="./img/cards/7 черви.png" alt="7 черви"></div>
+<div class="game__cards-item"><img src="./img/cards/6 черви.png" alt="6 черви"></div>
+<div class="game__cards-item"><img src="./img/cards/туз бубны.png" alt="туз бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/король бубны.png" alt="король бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/дама бубны.png" alt="дама бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/валет бубны.png" alt="валет бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/10 бубны.png" alt="10 бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/9 бубны.png" alt="9 бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/8 бубны.png" alt="8 бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/7 бубны.png" alt="7 бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/6 бубны.png" alt="6 бубны"></div>
+<div class="game__cards-item"><img src="./img/cards/туз крести.png" alt="туз крести"></div>
+<div class="game__cards-item"><img src="./img/cards/король крести.png" alt="король крести"></div>
+<div class="game__cards-item"><img src="./img/cards/дама крести.png" alt="дама крести"></div>
+<div class="game__cards-item"><img src="./img/cards/валет крести.png" alt="валет крести"></div>
+<div class="game__cards-item"><img src="./img/cards/10 крести.png" alt="10 крести"></div>
+<div class="game__cards-item"><img src="./img/cards/9 крести.png" alt="9 крести"></div>
+<div class="game__cards-item"><img src="./img/cards/8 крести.png" alt="8 крести"></div>
+<div class="game__cards-item"><img src="./img/cards/7 крести.png" alt="7 крести"></div>
+<div class="game__cards-item"><img src="./img/cards/6 крести.png" alt="6 крести"></div>
+</div>
+</div>`;
+
 function renderGame(level) {
     let appHtml;
     if (level === "1") {
-        appHtml = `
-     <div >
-       <h2 class="text center">Лёгкий уровень сложности</h2> 
-     </div>`;
+        appHtml = `${gameClosedCards}`;
     } else if (level === "2") {
-        appHtml = `
-     <div>
-        <h2 class="text center">Средний уровень сложности</h2>
-     </div>`;
+        appHtml = `${gameOpenCards}`;
     } else {
         appHtml = `
      <div>
